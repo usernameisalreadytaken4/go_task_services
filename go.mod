9c9b64bb7ced5e1f5455eca762d74af32971e978
Roman Bobrovskiy <romul1988@gmail.com>
2026-02-04 13:40:18 +0300

    add task service_mock.go

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/usernameisalreadytaken4/go_task_services
 go 1.25.5
 
 require (
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.8.0 // indirect
--- internal/task/v1/service.go
+++ internal/task/v1/service.go
@@ -1,13 +1,26 @@
 package task
 
+import "errors"
+
 type service struct {
 	repo Repository
 }
 
+func (s *service) Create(task *Task) error {
+	return nil
+}
+func (s *service) Get(ID int) (*Task, error) {
+	return nil, errors.New("fail")
+}
+
+func (s *service) GetByUserID(userID int) ([]*Task, error) {
+	return nil, errors.New("fail")
+}
+
 type Service interface {
-	// Create(*Task) error
-	// Get(int) (*Task, error)
-	// GetByUserID(int) ([]*Task, error)
+	Create(*Task) error
+	Get(int) (*Task, error)
+	GetByUserID(int) ([]*Task, error)
 }
 
 func NewService(repo Repository) Service {
